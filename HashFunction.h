fe032f1bd211d03ac9309c2de8d892c5bfce3fcf
Christos Dimitroulis <christos.dimitroulis@gmail.com>
2018-10-23 12:00:40 +0300

    add abstract hash function class

--- HashFunction.h
+++ HashFunction.h
@@ -5,8 +5,17 @@
 #ifndef PROJECT2018_HASHFUNCTION_H
 #define PROJECT2018_HASHFUNCTION_H
 
+#include <vector>
 
+template<class T>
 class HashFunction {
+public:
+    HashFunction(int d) : d(d) {}
+
+    virtual long getKey(std::vector<T> p) = 0;
+
+protected:
+    int d;
 
 };
 
